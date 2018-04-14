fdd68a3da43188b217a9bebad19eb89cb6b9c54f
Akmal <Akmal@DESKTOP-E3L3V9K>
2018-04-14 15:30:36 +0500

    Add project files.

---
+++ github first project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace github_first_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
